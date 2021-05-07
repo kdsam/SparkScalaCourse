1b445ac5f3963ea36ca83348ad4a143be513853c
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2021-05-07 21:50:21 +0800

    Sorting the Word Count Results

--- src/main/scala/com/sundogsoftware/spark/WordCountBetterSorted.scala
+++ src/main/scala/com/sundogsoftware/spark/WordCountBetterSorted.scala
@@ -3,8 +3,8 @@ package com.sundogsoftware.spark
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkContext
 
-
-object WordCountBetter {
+/** Count up how many of each word occurs in a book, using regular expressions and sorting the final results */
+object WordCountBetterSorted {
 
   /** Our main function where the action happens */
   def main(args: Array[String]): Unit = {
@@ -13,7 +13,7 @@ object WordCountBetter {
     Logger.getLogger("org").setLevel(Level.ERROR)
 
     // Create a SparkContext using every core of the local machine
-    val sc = new SparkContext("local[*]", "WordCountBetter")
+    val sc = new SparkContext("local[*]", "WordCountBetterSorted")
 
     // Read each line of my book into an RDD
     val input = sc.textFile("data/book.txt")
@@ -25,11 +25,16 @@ object WordCountBetter {
     val lowercaseWords = words.map(x => x.toLowerCase())
 
     // Count up the occurrences of each word
-    val wordCounts = lowercaseWords.countByValue()
+    val wordCounts = lowercaseWords.map(x => (x, 1)).reduceByKey((x, y) => x + y)
 
-    // Print the results.
-    wordCounts.foreach(println)
+    // Flip (word, count) tuples to (count, word) and then sort by key (the counts)
+    val wordCountsSorted = wordCounts.map( x => (x._2, x._1)).sortByKey()
 
+    // Print the results, flipping the (count, word) result to word: count as we go.
+    for (result <- wordCountsSorted) {
+      val count = result._1
+      val word = result._2
+      println(s"$word: $count")
+    }
   }
-
 }
